7b87e71ac84b39ca5991affb7258b08ea3556f45
David Chavez <david@dcvz.io>
2025-06-05 19:38:06 +0200

    Add main menu

--- examples/platformer-2d/rust/src/lib.rs
+++ examples/platformer-2d/rust/src/lib.rs
@@ -1,20 +1,28 @@
-#![allow(clippy::type_complexity)]
-
-use bevy::prelude::*;
+use bevy::{prelude::*, state::app::StatesPlugin};
+use bevy_asset_loader::loading_state::{LoadingState, LoadingStateAppExt};
 use godot_bevy::prelude::{
     godot_prelude::{gdextension, ExtensionLibrary},
     *,
 };
 
+mod main_menu;
+
 #[bevy_app]
 fn build_app(app: &mut App) {
-    app.add_plugins(TimingTestPlugin);
+    app.add_plugins(StatesPlugin)
+    .init_state::<GameState>()
+    .add_loading_state(
+        LoadingState::new(GameState::Loading)
+            .continue_to_state(GameState::MainMenu)
+    )
+    .add_plugins(main_menu::MainMenuPlugin);
 }
 
-struct TimingTestPlugin;
-
-impl Plugin for TimingTestPlugin {
-    fn build(&self, app: &mut App) {
-        app;
-    }
+#[derive(Debug, Default, Clone, Eq, PartialEq, Hash, States)]
+enum GameState {
+    #[default]
+    Loading,
+    MainMenu,
+    InGame,
+    GameOver,
 }
---
+++ examples/platformer-2d/rust/src/main_menu.rs
@@ -0,0 +1,101 @@
+use bevy::{
+    app::prelude::*,
+    ecs::{event::EventReader, resource::Resource, schedule::IntoScheduleConfigs, system::{Res, ResMut}},
+    state::{condition::in_state, state::{NextState, OnExit}},
+};
+use godot::classes::{display_server::WindowMode, Button, DisplayServer};
+use godot_bevy::{prelude::*, utils::print_scene_tree};
+
+use crate::GameState;
+
+#[derive(Resource, Default)]
+pub struct MenuAssets {
+    pub start_button: Option<GodotNodeHandle>,
+    pub fullscreen_button: Option<GodotNodeHandle>,
+    pub quit_button: Option<GodotNodeHandle>,
+}
+
+pub struct MainMenuPlugin;
+impl Plugin for MainMenuPlugin {
+    fn build(&self, app: &mut App) {
+        app.init_resource::<MenuAssets>()
+            .add_systems(
+                OnExit(GameState::Loading),
+                (init_menu_assets, connect_buttons.after(init_menu_assets)),
+            )
+            .add_systems(
+                Update,
+                listen_for_button_press.run_if(in_state(GameState::MainMenu)),
+            );
+    }
+}
+
+#[derive(NodeTreeView)]
+pub struct MenuUi {
+    #[node("/root/MainMenu/Options/StartButton")]
+    pub start_button: GodotNodeHandle,
+
+    #[node("/root/MainMenu/Options/FullscreenButton")]
+    pub fullscreen_button: GodotNodeHandle,
+
+    #[node("/root/MainMenu/Options/QuitButton")]
+    pub quit_button: GodotNodeHandle,
+}
+
+fn init_menu_assets(mut menu_assets: ResMut<MenuAssets>, mut scene_tree: SceneTreeRef) {
+    let menu_ui = MenuUi::from_node(scene_tree.get().get_root().unwrap());
+
+    menu_assets.start_button = Some(menu_ui.start_button.clone());
+    menu_assets.fullscreen_button = Some(menu_ui.fullscreen_button.clone());
+    menu_assets.quit_button = Some(menu_ui.quit_button.clone());
+}
+
+fn connect_buttons(mut menu_assets: ResMut<MenuAssets>, mut scene_tree: SceneTreeRef) {
+    connect_godot_signal(
+        menu_assets.start_button.as_mut().unwrap(),
+        "pressed",
+        &mut scene_tree,
+    );
+    connect_godot_signal(
+        menu_assets.fullscreen_button.as_mut().unwrap(),
+        "pressed",
+        &mut scene_tree,
+    );
+    connect_godot_signal(
+        menu_assets.quit_button.as_mut().unwrap(),
+        "pressed",
+        &mut scene_tree,
+    );
+}
+
+fn listen_for_button_press(
+    menu_assets: Res<MenuAssets>,
+    mut events: EventReader<GodotSignal>,
+    mut app_state: ResMut<NextState<GameState>>,
+) {
+    for evt in events.read() {
+        if evt.name == "pressed" && &evt.target == menu_assets.start_button.as_ref().unwrap() {
+            println!("Start button pressed");
+            app_state.set(GameState::InGame);
+            // get_tree().change_scene_to_file("res://level_1.tscn")
+            if let Some(mut tree) = evt.target.clone().get::<Button>().get_tree() {
+                tree.change_scene_to_packed(packed_scene);
+            }
+
+        }
+        if evt.name == "pressed" && &evt.target == menu_assets.fullscreen_button.as_ref().unwrap() {
+            println!("Fullscreen button pressed");
+            if DisplayServer::singleton().window_get_mode() == WindowMode::FULLSCREEN {
+                DisplayServer::singleton().window_set_mode(WindowMode::WINDOWED);
+            } else if DisplayServer::singleton().window_get_mode() == WindowMode::WINDOWED {
+                DisplayServer::singleton().window_set_mode(WindowMode::FULLSCREEN);
+            }
+        }
+        if evt.name == "pressed" && &evt.target == menu_assets.quit_button.as_ref().unwrap() {
+            println!("Quit button pressed");
+            if let Some(mut tree) = evt.target.clone().get::<Button>().get_tree() {
+                tree.quit();
+            }
+        }
+    }
+}
