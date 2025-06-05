7e8e8c896264a742f7a6159e0d6243640a1228d4
David Chavez <david@dcvz.io>
2025-06-05 21:12:57 +0200

    Custom player node

--- examples/platformer-2d/rust/src/gameplay/player.rs
+++ examples/platformer-2d/rust/src/gameplay/player.rs
@@ -1,5 +1,28 @@
 use bevy::app::{App, Plugin};
+use godot::{classes::{CharacterBody2D, ICharacterBody2D}, prelude::*};
 
+#[derive(GodotClass)]
+#[class(base=CharacterBody2D)]
+pub struct Player2D {
+    base: Base<CharacterBody2D>,
+    #[export]
+    speed: f32,
+    #[export]
+    jump_velocity: f32,
+}
+
+#[godot_api]
+impl ICharacterBody2D for Player2D {
+    fn init(base: Base<CharacterBody2D>) -> Self {
+        Self {
+            base,
+            speed: 250.,
+            jump_velocity: -400.,
+        }
+    }
+
+    fn ready(&mut self) {}
+}
 
 pub struct PlayerPlugin;
 
