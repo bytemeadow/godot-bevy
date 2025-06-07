87ec764a3b57b73407d080f07018db1e2be7da7f
David Chavez <david@dcvz.io>
2025-06-07 20:54:09 +0200

    prelude cleanup

--- godot-bevy/src/lib.rs
+++ godot-bevy/src/lib.rs
@@ -7,31 +7,7 @@ pub mod app;
 pub mod bridge;
 pub mod node_tree_view;
 pub mod plugins;
-pub mod prelude {
-    pub use crate::bridge::*;
-    pub use crate::node_tree_view::NodeTreeView;
-    pub use crate::plugins::{
-        assets::{GodotAssetsPlugin, GodotResource},
-        audio::{
-            Audio, AudioApp, AudioChannel, AudioChannelMarker, AudioEasing, AudioError,
-            AudioOutput, AudioPlayerType, AudioSettings, AudioTween, GodotAudioChannels,
-            GodotAudioPlugin, MainAudioTrack, PlayAudioCommand, SoundId,
-        },
-        core::{
-            ActionInput, Collisions, FindEntityByNameExt, GodotCorePlugin, GodotSignal, Groups,
-            KeyboardInput, MouseButtonInput, MouseMotion, PhysicsDelta, PhysicsUpdate,
-            SceneTreeEventReader, SceneTreeRef, SystemDeltaTimer, Transform2D, Transform3D,
-            collisions::{
-                ALL_COLLISION_SIGNALS, AREA_ENTERED, AREA_EXITED, BODY_ENTERED, BODY_EXITED,
-                COLLISION_END_SIGNALS, COLLISION_START_SIGNALS,
-            },
-            connect_godot_signal,
-        },
-        packed_scene::{GodotScene, PackedScenePlugin},
-    };
-    pub use godot::prelude as godot_prelude;
-    pub use godot_bevy_macros::*;
-}
+pub mod prelude;
 pub mod utils;
 pub mod watchers;
 
