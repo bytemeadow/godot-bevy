c0533819177384c47b96e2ee10f00d2d267b12c9
DragonAxe <DragonAxe@users.noreply.github.com>
2025-07-01 18:55:05 -0600

    Add support for uid:// Godot resource paths

--- godot-bevy/src/plugins/core.rs
+++ godot-bevy/src/plugins/core.rs
@@ -108,6 +108,21 @@ impl Plugin for GodotBaseCorePlugin {
             AssetSource::build()
                 .with_reader(|| Box::new(crate::plugins::assets::GodotAssetReader::new())),
         );
+        app.register_asset_source(
+            AssetSourceId::new(Some("res")),
+            AssetSource::build()
+                .with_reader(|| Box::new(crate::plugins::assets::GodotAssetReader::new())),
+        );
+        app.register_asset_source(
+            AssetSourceId::new(Some("user")),
+            AssetSource::build()
+                .with_reader(|| Box::new(crate::plugins::assets::GodotAssetReader::new())),
+        );
+        app.register_asset_source(
+            AssetSourceId::new(Some("uid")),
+            AssetSource::build()
+                .with_reader(|| Box::new(crate::plugins::assets::GodotAssetReader::new())),
+        );
 
         app.add_plugins(MinimalPlugins.build().disable::<ScheduleRunnerPlugin>())
             // Configure AssetPlugin to bypass path verification for Godot resources
