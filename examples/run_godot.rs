edc7fa87076b7b4acf27540615476b001d7d6398
David Chavez <david@dcvz.io>
2025-08-09 16:05:31 +0200

    cargo fix

--- examples/run_godot.rs
+++ examples/run_godot.rs
@@ -86,11 +86,10 @@ fn godot_binary_path() -> PathBuf {
     // of godot, e.g., C:\\Program Files\\Godot\\Godot_v3.4.2-stable_win64.exe
     let godot_search_paths = "/usr/local/bin:/usr/bin:/bin:/Applications/Godot.app/Contents/MacOS";
 
-    if let Ok(path_it) = which_in_global("godot", Some(godot_search_paths)) {
-        if let Some(godot_binary_path) = path_it.into_iter().next() {
+    if let Ok(path_it) = which_in_global("godot", Some(godot_search_paths))
+        && let Some(godot_binary_path) = path_it.into_iter().next() {
             return godot_binary_path;
         }
-    }
 
     panic!(
         "Couldn't find the godot binary in your environment's path or in default search locations ({godot_search_paths:?})"
