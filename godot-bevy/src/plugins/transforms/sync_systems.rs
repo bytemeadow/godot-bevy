1689aae8ad054fabc5759f5baff4906435c754de
996666925 <996666925@qq.com>
2025-10-09 21:14:44 +0800

    fix error

--- godot-bevy/src/plugins/transforms/sync_systems.rs
+++ godot-bevy/src/plugins/transforms/sync_systems.rs
@@ -7,8 +7,8 @@ use bevy::ecs::query::{AnyOf, Changed};
 use bevy::ecs::system::{Query, SystemChangeTick};
 use bevy::prelude::Transform as BevyTransform;
 use godot::classes::{Engine, Node2D, Node3D, Object, SceneTree};
-use godot::prelude::{Gd, ToGodot};
 use godot::obj::Singleton;
+use godot::prelude::{Gd, ToGodot};
 
 use super::change_filter::TransformSyncMetadata;
 
