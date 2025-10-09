68629c3b72443dec727547212529a9943618df5e
David Chavez <david@dcvz.io>
2025-10-09 15:14:38 +0200

    Remove all previous test stuff

--- godot-bevy/tests/transforms/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Transform synchronization tests
-//!
-//! Comprehensive tests for transform sync between Godot and Bevy
-//! organized by functionality and test purpose.
-
-pub mod hierarchy;
-pub mod sync_modes;
-pub mod transform_initialization;
-pub mod utils;
